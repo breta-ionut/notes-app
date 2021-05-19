7b839517f6e2b72ede557de0d8f0236ae4e3d631
Ionut Breta <breta.ionut@gmail.com>
2021-05-19 22:39:41 +0300

    - use only fully-qualified names for global functions

--- public/index.php
+++ public/index.php
@@ -6,10 +6,10 @@ use App\Kernel;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require \dirname(__DIR__).'/vendor/autoload.php';
 
 if ($_SERVER['APP_DEBUG']) {
-    umask(0000);
+    \umask(0000);
 
     Debug::enable();
 }
