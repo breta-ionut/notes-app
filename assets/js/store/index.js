353aa58e2b48298422cc770f97c37b427ba69c8e
Ionut Breta <breta.ionut@gmail.com>
2021-05-22 18:44:19 +0300

    - added note store

--- assets/js/store/index.js
+++ assets/js/store/index.js
@@ -1,9 +1,11 @@
 import {createStore} from 'vuex'
 
+import note from './modules/note.js'
 import user from './modules/user.js'
 
 export default createStore({
     modules: {
+        note,
         user,
     },
 })
---
+++ assets/js/store/modules/note.js
@@ -0,0 +1,93 @@
+import noteApi from '../../api/note.js'
+import note from '../../api/note.js'
+import Note from '../../models/Note.js'
+import NoteCollection from '../../models/NoteCollection.js'
+
+/**
+ * @throws {Error}
+ */
+const ensureNotesAreLoaded = state => {
+    if (!state.notes) {
+        throw new Error('The notes are not loaded yet.')
+    }
+}
+
+export default {
+    namespaced: true,
+
+    state: () => ({
+        /**
+         * @type {NoteCollection|null}
+         */
+        notes: null,
+    }),
+
+    getters: {
+        /**
+         * @return {Note[]}
+         */
+        all: state => state.notes.all(),
+    },
+
+    mutations: {
+        /**
+         * @param {Object} state
+         * @param {Note} note
+         */
+        add(state, note) {
+            ensureNotesAreLoaded(state)
+
+            state.notes.add(note)
+        },
+
+        /**
+         * @param {Object} state
+         * @param {Note} note
+         */
+        remove(state, note) {
+            ensureNotesAreLoaded(state)
+
+            state.notes.remove(note)
+        },
+
+        /**
+         * @param {Object} state
+         * @param {NoteCollection} notes
+         */
+        set(state, notes) {
+            state.notes = notes
+        },
+    },
+
+    actions: {
+        /**
+         * @param {Function} commit
+         * @param {Note} note
+         */
+        async create({commit}, note) {
+            commit('add', await noteApi.create(note))
+        },
+
+        /**
+         * @param {Function} commit
+         * @param {Note} note
+         */
+        async update({commit}, note) {
+            commit('add', await noteApi.update(note))
+        },
+
+        /**
+         * @param {Function} commit
+         * @param {Note} note
+         */
+        async delete({commit}, note) {
+            await noteApi.delete(note)
+
+            commit('remove', note)
+        },
+
+        async load({commit}) {
+            commit('set', await noteApi.list())
+        },
+    },
+}
